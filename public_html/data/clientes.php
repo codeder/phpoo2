97cbf09114a72325c7f6b91b8fe533b84b9f646e
Eder Silva <contato@edersilva.com>
2014-07-31 22:51:15 -0300

    Bootstrap e formatacao css

--- public_html/data/clientes.php
+++ public_html/data/clientes.php
@@ -1,6 +1,9 @@
 <?php
 
-$descPadrao = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+$descPadrao = "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>";
+$descPadrao .= "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>";
+
+
 
 $clientes[0] = new Fisica(1, null, "Eder da Silva", "305.132.018.82", "Masculino");
 $clientes[0]->setFoto("http://placehold.it/130x130");
@@ -12,59 +15,59 @@ $clientes[0]->setEndCobranca(new Endereco("Rua Amador Costa e Souza, 31", "Cruze
 $clientes[1] = new Juridica(2, null, "Ednelson Prado ME", "Vincere Comunicação", "51.829.016/0001-42");
 $clientes[1]->setFoto("http://placehold.it/130x130");
 $clientes[1]->setDescricao($descPadrao);
-$clientes[1]->setPercentual(85);
+$clientes[1]->setPercentual(30);
 $clientes[1]->setEnd(new Endereco("Rua Livre das Flores, 58", "Lavrinhas", "SP"));
 $clientes[1]->setEndCobranca(new Endereco("Mário das Flores, 38", "Taubaté", "SP"));
 
 $clientes[2] = new Fisica(3, null, "Angela Maria de Abreu Oliveira", "888.888.888.88", "Feminino");
 $clientes[2]->setFoto("http://placehold.it/130x130");
 $clientes[2]->setDescricao($descPadrao);
-$clientes[2]->setPercentual(100);
+$clientes[2]->setPercentual(59);
 $clientes[2]->setEnd(new Endereco("Maurelino Vicentino Souza, 200", "Rio de Janeiro", "RJ"));
 $clientes[2]->setEndCobranca(new Endereco("Valfredo Tomas Nóbrega, 71", "Rio de Janeiro", "RJ"));
 
 $clientes[3] = new Juridica(4, null, "Solange Gomes ME", "Padaria São Carlos", "96.458.963/0001-25");
 $clientes[3]->setFoto("http://placehold.it/130x130");
 $clientes[3]->setDescricao($descPadrao);
-$clientes[3]->setPercentual(100);
+$clientes[3]->setPercentual(85);
 $clientes[3]->setEnd(new Endereco("Rua Juazeiro do Norte, 85", "Passa Quatro", "MG"));
 
 $clientes[4] = new Fisica(5, null, "Florentina de Jesus", "999.999.999.99", "Feminino");
 $clientes[4]->setFoto("http://placehold.it/130x130");
 $clientes[4]->setDescricao($descPadrao);
-$clientes[4]->setPercentual(39);
+$clientes[4]->setPercentual(40);
 $clientes[4]->setEnd(new Endereco("Rua dos Conjolos Sumaré, 1000", "Baependí", "MG"));
 
 $clientes[5] = new Fisica(6, null, "Luana Alves", "777.777.777.77", "Feminino");
 $clientes[5]->setFoto("http://placehold.it/130x130");
 $clientes[5]->setDescricao($descPadrao);
-$clientes[5]->setPercentual(75);
+$clientes[5]->setPercentual(20);
 $clientes[5]->setEnd(new Endereco("Rua Crispim Gonçalves", "Lavrinhas", "SP"));
 
 $clientes[6] = new Juridica(7, null, "Luciana Mendes ME", "Software & CIA Ltda", "56.689.015/0001-66");
 $clientes[6]->setFoto("http://placehold.it/130x130");
 $clientes[6]->setDescricao($descPadrao);
-$clientes[6]->setPercentual(29);
+$clientes[6]->setPercentual(100);
 $clientes[6]->setEnd(new Endereco("Rua Zezé Valadão, 71", "Aparecida", "SP"));
 $clientes[6]->setEndCobranca(new Endereco("Vila do Chaves, 71", "Tangamandápio", "SP"));
 
 $clientes[7] = new Juridica(8, null, "Wesley Willians ME", "Code Education", "85.789.963/0001-63");
 $clientes[7]->setFoto("http://placehold.it/130x130");
 $clientes[7]->setDescricao($descPadrao);
-$clientes[7]->setPercentual(40);
+$clientes[7]->setPercentual(60);
 $clientes[7]->setEnd(new Endereco("Rua Tancredo Neves, 96", "São Paulo", "SP"));
 $clientes[7]->setEndCobranca(new Endereco("Avenida Maria do Bairro, 80", "São Paulo", "SP"));
 
 $clientes[8] = new Fisica(9, null, "Tiririca de Jesus", "589.874.569.56", "Masculino");
 $clientes[8]->setFoto("http://placehold.it/130x130");
 $clientes[8]->setDescricao($descPadrao);
-$clientes[8]->setPercentual(50);
+$clientes[8]->setPercentual(48);
 $clientes[8]->setEnd(new Endereco("Rua Margarida Oculta, 080", "Cabo Frio", "RJ"));
 
 $clientes[9] = new Fisica(10, null, "Joaquim Alves", "456.874.563.85", "Masculino");
 $clientes[9]->setFoto("http://placehold.it/130x130");
 $clientes[9]->setDescricao($descPadrao);
-$clientes[9]->setPercentual(55);
+$clientes[9]->setPercentual(97);
 $clientes[9]->setEnd(new Endereco("Rua José Crispim Gonçalves", "Lavrinhas", "SP"));
 ?>
 
--- public_html/index.php
+++ public_html/index.php
@@ -8,116 +8,169 @@ require_once 'data/clientes.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Exercício2 - PHPOO - Code Education</title>
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício2 - PHPOO - Code Education</title>
 
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    </head>
-    <body>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-theme.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+</head>
+<body>
+
+
+    <div class="container">
+
+
+        <div class="page-title">
+            <h1>Clientes - Informações de conclusão dos projetos</h1>
+        </div>
+
 
         <?php
         if (!isset($_GET['id'])):
             ?>
 
-            <table border="1">
-                <thead>
-                    <tr>
-                        <th>ID</th>                                        
-                        <th>Foto</th>
-                        <th>Nome / Razão Social</th>
-                        <th>Tipo</th>
-                        <th>Andamento do projeto</th>
-                        <th>&nbsp;</th>
-                    </tr>
-                </thead>
-                <tbody>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th class="center">Código</th>                                        
+                    <th>Imagem</th>
+                    <th>Nome / Razão Social</th>
+                    <th class="left">Tipo</th>
+                    <th class="center">Progresso do projeto:</th>
+                    <th class="center">Informações do cliente</th>
+                </tr>
+            </thead>
+            <tbody>
 
-                    <?php
-                    foreach ($clientes as $cliente) {
+                <?php
+                foreach ($clientes as $cliente) {
 
-                        echo "<tr>";
-                        echo "<td>" . $cliente->getId() . "</td>";
-                        echo "<td><img src=\"" . $cliente->getFoto() . "\"/></td>";
-                        if ($cliente->getTipo() == "Fisica"):
-                            echo "<td>" . $cliente->getNome() . "</td>";
-                        else:
-                            echo "<td>" . $cliente->getRazaoSocial() . "</td>";
-                        endif;
-                        echo "<td>" . $cliente->getTipo() . "</td>";
-                        echo "<td>" . $cliente->getPercentual() . "</td>";
-                        echo '<td><a href="?id=' . $cliente->getId() . '">Editar</a></td>';
-                        echo "</tr>";
+                    if($cliente->getPercentual()<=30){
+                        $class = "progress-bar-danger";
+                    }elseif(($cliente->getPercentual()>=31) AND ($cliente->getPercentual()<=60)){
+                        $class = "progress-bar-warning";
+                    }else{
+                        $class = "progress-bar-success";
                     }
-                    ?>
 
 
-                </tbody>
-            </table>
+                    echo "<tr>";
+                    echo '<td class="center">' . $cliente->getId() . '</td>';
+                    echo "<td><img src=\"" . $cliente->getFoto() . "\"/></td>";
+                    if ($cliente->getTipo() == "Fisica"):
+                        echo "<td>" . $cliente->getNome() . "</td>";
+                    else:
+                        echo "<td>" . $cliente->getRazaoSocial() . "</td>";
+                    endif;
+                    echo '<td class="left">' . $cliente->getTipo() . '</td>';
+                    echo '<td class="center">';
+                    echo '<div class="progress">';
+                    echo '<div class="progress-bar '.$class.' progress-bar-striped" role="progressbar" aria-valuenow="'.$cliente->getPercentual().'" aria-valuemin="0" aria-valuemax="100" style="width: '.$cliente->getPercentual().'%">'; 
+                    echo '<span class="sr-only">'.$cliente->getPercentual().'%</span>';
+                    echo "</div>";
+                    echo "</div>";
+                    echo "</td>";
+                    echo '<td class="center"><a class="btn btn-default" href="?id=' . $cliente->getId() . '">Visualizar</a></td>';
+                    echo "</tr>";
+                }
+                ?>
 
-            <?php
+
+            </tbody>
+        </table>
+
+        <?php
         else:
             $id = $_GET['id'] - 1;
+        ?>
+
+        <table class="table table-bordered view-customer">
+            <tr>
+                <td colspan="2" rowspan="5" width="20%" class="center"><img src="<?php echo $clientes[$id]->getFoto(); ?>" /></td>
+                <th>Código:</th>
+                <td><?php echo $clientes[$id]->getId(); ?></td>
+            </tr>
+            <tr>
+                <th width="30%">Nome / Razão Social:</th>
+                <?php if ($clientes[$id]->getTipo() == "Fisica"): ?>
+                    <td width="50%"><?php echo $clientes[$id]->getNome(); ?></td>
+                <?php else: ?>
+                    <td width="50%"><?php echo $clientes[$id]->getRazaoSocial(); ?></td>
+                <?php endif; ?>
+            </tr>
+            <tr>
+                <th>CPF/CNPJ:</th>
+                <?php if ($clientes[$id]->getTipo() == "Fisica"): ?>
+                    <td><?php echo $clientes[$id]->getCpf(); ?></td>
+                <?php else: ?>
+                    <td><?php echo $clientes[$id]->getCnpj(); ?></td>
+                <?php endif; ?>
+            </tr>
+
+            <?php
+            if($clientes[$id]->getPercentual()<=30){
+                $class = "progress-bar-danger";
+            }elseif(($clientes[$id]->getPercentual()>=31) AND ($clientes[$id]->getPercentual()<=60)){
+                $class = "progress-bar-warning";
+            }else{
+                $class = "progress-bar-success";
+            }
             ?>
 
-            <table border="1" width="100%">
+            <tr>
+                <th>Tipo:</th>
+                <td><?php echo $clientes[$id]->getTipo(); ?></td>
+            </tr>
+            <tr>
+                <th>Andamento do projeto</th>
+                <td>
+                    <?php
+                    echo '<div class="progress pull-left">';
+                    echo '<div class="progress-bar '.$class.' progress-bar-striped" role="progressbar" aria-valuenow="'.$clientes[$id]->getPercentual().'" aria-valuemin="0" aria-valuemax="100" style="width: '.$clientes[$id]->getPercentual().'%">'; 
+                    echo '<span class="sr-only">'.$clientes[$id]->getPercentual().'%</span>';
+                    echo "</div>";
+                    echo "</div>";
+                    echo "</td>";
+                    ?>
+                </td>
+            </tr>            
+            <tr>
+                <th colspan="4">Descrição do Projeto</th>
+            </tr>
+            <tr>
+                <td colspan="4">
+                    <p><?php echo $clientes[$id]->getDescricao(); ?></p>
+                </td>
+            </tr>
+            <tr>
+                <th colspan="4">Endereço padrão:</th>
+            </tr>
+            <tr>
+                <td colspan="4"><?php echo $clientes[$id]->getEnd(); ?></td>
+            </tr>
+            <?php if ($clientes[$id]->getEndCobranca()): ?>
                 <tr>
-                    <td colspan="2" rowspan="5"><img src="<?php echo $clientes[$id]->getFoto(); ?>" /></td>
-                    <td>Código:</td>
-                    <td><?php echo $clientes[$id]->getId(); ?></td>
+                    <th colspan="4">Endereço de cobrança:</th>
                 </tr>
                 <tr>
-                    <td>Nome / Razão Social:</td>
-                    <?php if ($clientes[$id]->getTipo() == "Fisica"): ?>
-                        <td><?php echo $clientes[$id]->getNome(); ?></td>
-                    <?php else: ?>
-                        <td><?php echo $clientes[$id]->getRazaoSocial(); ?></td>
-                    <?php endif; ?>
+                    <td colspan="4"><?php echo $clientes[$id]->getEndCobranca(); ?></td>
                 </tr>
-                <tr>
-                    <td>CPF/CNPJ:</td>
-                    <?php if ($clientes[$id]->getTipo() == "Fisica"): ?>
-                        <td><?php echo $clientes[$id]->getCpf(); ?></td>
-                    <?php else: ?>
-                        <td><?php echo $clientes[$id]->getCnpj(); ?></td>
-                    <?php endif; ?>
-                </tr>
-                <tr>
-                    <td>Andamento do projeto</td>
-                    <td><?php echo $clientes[$id]->getPercentual(); ?></td>
-                </tr>
-                <tr>
-                    <td>Tipo:</td>
-                    <td><?php echo $clientes[$id]->getTipo(); ?></td>
-                </tr>
-                <tr>
-                    <td colspan="4">Descrição do Projeto</td>
-                </tr>
-                <tr>
-                    <td colspan="4">
-                        <p><?php echo $clientes[$id]->getDescricao(); ?></p>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="4">Endereço padrão:</td>
-                </tr>
-                <tr>
-                    <td colspan="4"><?php echo $clientes[$id]->getEnd(); ?></td>
-                </tr>
-                <?php if ($clientes[$id]->getEndCobranca()): ?>
-                    <tr>
-                        <td colspan="4">Endereço de cobrança:</td>
-                    </tr>
-                    <tr>
-                        <td colspan="4"><?php echo $clientes[$id]->getEndCobranca(); ?></td>
-                    </tr>
-                <?php endif; ?>
-            </table>
+            <?php endif; ?>
+        </table>
+        <a href="/" class="btn btn-default btn-lg">Voltar</a>
 
-        <?php endif; ?>
+    <?php endif; ?>
 
-    </body>
+
+</div>
+
+
+</body>
 </html>
 
